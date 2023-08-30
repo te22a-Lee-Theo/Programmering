8291c0dd358f11300b5beba6d5810746048492bc
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-08-30 10:38:38 +0200

    Initial commit

---
+++ Hello/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hej, vad heter du?");
+
+// string name = "Theo";
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hej " + name + "! HUr mår du idag?");
+Console.WriteLine($"Hejdå {name}. Vi ses imorgon.");
+
+Console.ReadLine();
---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+string age = Console.ReadLine();
+
+Console.WriteLine("In which country do you live?");
+string country = Console.ReadLine();
+
+Console.WriteLine($"Hello, my name is {name}. I'm {age} years old and I live in {country}.");
+
+Console.ReadLine();
